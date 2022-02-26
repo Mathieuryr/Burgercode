abc32754fdc35e65c695363232f2857d0cae658b
Mathieuryr <97654258+Mathieuryr@users.noreply.github.com>
2022-02-26 13:01:44 +0100

    Add files via upload

---
+++ admin/database.php
@@ -0,0 +1,36 @@
+<?php
+
+class Database
+{
+    private static $dbHost = "c1726689c@localhost";
+    private static $dbName = "c1726689_burgercode";
+    private static $dbUsername = "root";
+    private static $dbUserpassword = "";
+    
+    private static $connection = null;
+    
+    public static function connect()
+    {
+        if(self::$connection == null)
+        {
+            try
+            {
+              self::$connection = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName , self::$dbUsername, self::$dbUserpassword);
+            }
+            catch(PDOException $e)
+            {
+                die($e->getMessage());
+            }
+        }
+        return self::$connection;
+    }
+    
+    public static function disconnect()
+    {
+        self::$connection = null;
+    }
+
+}
+
+$con=mysqli_connect("example.com.mysql", "username", "password", "database");
+?>
